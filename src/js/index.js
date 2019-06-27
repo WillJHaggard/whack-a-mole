1fea4c3137f84abbf46cdf21f37d2dfd61eadd01
Will Haggard <willjhaggard@gmail.com>
2019-06-27 13:43:17 -0400

    styled markup; pseudocode for js

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+console.log('game init')
+
+/**
+ * Whack-A-Mole
+ *
+ * organize for simplicity and organization
+ * Object composition or class es6 (prototypal) for Game and Timer
+ *
+ * pseudo-code
+ * 1. init state for game
+ * 2. build functions that change state
+ * 3. build public api that calls state setters
+ * 4. attach dom events and handlers
+ * 5. build pausable timer
+ * 6. initialize together and expose public api for use with buttons
+ */
